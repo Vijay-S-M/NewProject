e82dd4444370c7f0145dd6094095303f9bc5278a
vijay <tnvijay2006@gmail.com>
2026-01-23 13:03:20 +0530

    First Demo code for github

---
+++ AnonymousArray.java
@@ -0,0 +1,12 @@
+package HWForJava;
+public class AnonymousArray{
+  public static void main(String[] args) {
+    AnonymousArray.sum(new int[]{1,2,3,4});
+
+  }
+  public static void sum(int[] args) {
+    int total=0;
+
+    
+  }
+}
